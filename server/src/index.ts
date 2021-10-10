790ce4ced04724ff2d7362b2c26de83abd0fe7e4
VanVSJobs <tranquocvan2402@gmail.com>
2021-10-10 05:50:01 -0600

    played around graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,27 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { listings } from "./listings";
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
-const port = 9000;
 
-app.use(bodyParser.json());
+async function startApolloServer(typeDefs: any, resolvers: any) {
+  const port = 9000;
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const app = express();
+  await server.start();
+  server.applyMiddleware({ app, path: '/api' });
 
-app.get("/", (_req, res) => res.send("Hello World!"));
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}${server.graphqlPath}`);
+  });
+}
 
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-});
+startApolloServer(schema);
 
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
 
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-  return res.send("failed to delete listing");
-});
 
-app.listen(port);
+
+
+
 
 console.log(`[app]: http://localhost:${port}`);
